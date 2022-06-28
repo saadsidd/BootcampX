5ca0e95252fc6523f4bb6cd4281782e565048ac5
Saad Siddiq <siddiq.saad@gmail.com>
2022-06-27 23:08:20 -0500

    queries join examples

---
+++ 2_queries_joins/0_rollover_students.sql
@@ -0,0 +1,7 @@
+SELECT students.name AS student_name,
+       students.start_date AS student_start_date,
+       cohorts.name AS cohort_name,
+       cohorts.start_date AS cohort_start_date
+FROM students JOIN cohorts ON cohorts.id = cohort_id
+WHERE students.start_date <> cohorts.start_date
+ORDER BY cohorts.start_date;
---
+++ 2_queries_joins/1_total_assignment_time_specific_student.sql
@@ -0,0 +1,3 @@
+SELECT sum(duration) AS total_duration
+FROM assignment_submissions JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel';
