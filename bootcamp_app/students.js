db89af4ad9c795a706ea332b67d4a55ae77723f4
Saad Siddiq <siddiq.saad@gmail.com>
2022-06-30 04:27:06 -0500

    parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,12 +11,18 @@ pool.connect(() => {
   console.log('Connected to database');
 });
 
-pool.query(`
-  SELECT students.id, students.name AS student_name, cohorts.name AS cohort_name
-  FROM students JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
-`)
+
+const queryString = `
+    SELECT students.id, students.name AS student_name, cohorts.name AS cohort_name
+    FROM students JOIN cohorts ON cohorts.id = cohort_id
+    WHERE cohorts.name LIKE $1
+    LIMIT $2;
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.student_name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,15 +11,20 @@ pool.connect(() => {
   console.log('Connected to database');
 });
 
-pool.query(`
-  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-  FROM teachers
-        JOIN assistance_requests ON teachers.id = teacher_id
-        JOIN students ON students.id = student_id
-        JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-  ORDER BY teachers.name
-`)
+
+const queryString = `
+    SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+    FROM teachers
+          JOIN assistance_requests ON teachers.id = teacher_id
+          JOIN students ON students.id = student_id
+          JOIN cohorts ON cohorts.id = cohort_id
+    WHERE cohorts.name = $1
+    ORDER BY teachers.name;
+`;
+const cohortName = process.argv[2] || 'JUL02';
+const values = [cohortName];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
